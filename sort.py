4dc9cd85de05b377930a34cf368147434b038d82
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 15:03:23 +0100

    userinput eingerichtet

--- sort.py
+++ sort.py
@@ -1,5 +1,10 @@
+#Eingabe von Zahlen durch User
+print("Zahlen mit Komma getrennt eingeben:")
+s = input()
+zahlen = [int(x) for x in s.split(',') if x != '']
+
 # Eine Liste von Zahlen
-zahlen: list[int] = [34, 1, 56, 23, 89, 12, 7, 13, 99, 0]
+#zahlen: list[int] = [34, 1, 56, 23, 89, 12, 7, 13, 99, 0]
 
 # Sortiere die Liste in aufsteigender Reihenfolge
 zahlen.sort()
