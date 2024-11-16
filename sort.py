18a485dfbb3209ba5b5c3b2b18f63b9e459e9f84
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 13:57:28 +0100

    Code für umgekehrte Sortierung hinzugefügt reverse=True

--- sort.py
+++ sort.py
@@ -4,5 +4,8 @@ zahlen: list[int] = [34, 1, 56, 23, 89, 12, 7, 13, 99, 0]
 # Sortiere die Liste in aufsteigender Reihenfolge
 zahlen.sort()
 
+# Sortieren in umgekehrter Reihenfolge
+zahlen.sort(reverse=True)
+
 # Ausgabe der sortierten Liste
 print("Sortierte Liste (aufsteigend):", zahlen)
