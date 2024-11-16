cb160e7511c386e0ac88c5eed9ac1d84bd39fb2c
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 16:27:13 +0100

    insertionsort implementiert

--- UserInput.py
+++ UserInput.py
@@ -1,15 +1,19 @@
 from sort import sort
+from sort import insertionsort
 
 #Eingabe von Zahlen durch User
-print("Zahlen mit Komma getrennt eingeben:")
-s = input()
-zahlen = [int(x) for x in s.split(',') if x != '']
+#print("Zahlen mit Komma getrennt eingeben:")
+#s = input()
+#zahlen = [int(x) for x in s.split(',') if x != '']
+zahlen = [1,3,2]
 
 # Sortiere die Liste in aufsteigender Reihenfolge
 sortierteZahlen = sort(zahlen)
+sortierteZahlen2 = insertionsort(zahlen)
 
 # Ausgabe der sortierten Liste
-print("Sortierte Liste (aufsteigend):", sortierteZahlen)
+print("Sortierte Liste (sort):", sortierteZahlen)
+print("Sortierte Liste (insertionsort):", sortierteZahlen2)
 
 # Sortieren in umgekehrter Reihenfolge
 #zahlen.sort(reverse=True)
