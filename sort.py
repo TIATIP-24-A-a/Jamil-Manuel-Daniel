4ce05fc583b13e643ede656cab4a043d80eb0112
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 15:16:38 +0100

    sort definiert und Rückgabe der sortierten Zahlen

--- sort.py
+++ sort.py
@@ -1,19 +1,4 @@
-#Eingabe von Zahlen durch User
-print("Zahlen mit Komma getrennt eingeben:")
-s = input()
-zahlen = [int(x) for x in s.split(',') if x != '']
+def sort(a):
+    return sorted(a)
 
-# Eine Liste von Zahlen
-#zahlen: list[int] = [34, 1, 56, 23, 89, 12, 7, 13, 99, 0]
 
-# Sortiere die Liste in aufsteigender Reihenfolge
-zahlen.sort()
-
-# Ausgabe der sortierten Liste
-print("Sortierte Liste (aufsteigend):", zahlen)
-
-# Sortieren in umgekehrter Reihenfolge
-zahlen.sort(reverse=True)
-
-# Ausgabe der sortierten Liste
-print("Sortierte Liste (absteigend):", zahlen)
