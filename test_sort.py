e951c2b1879d76efc84095ff37f7b7648f36189a
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 15:17:19 +0100

    Unittest für Sortieralgorithmus aufgebaut

---
+++ test_sort.py
@@ -0,0 +1,17 @@
+import unittest
+from sort import sort
+
+
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        # using unittest
+        a = [3, 0, 12, 8]
+        b = sort(a)
+        self.assertEqual(b, [0, 3, 8, 12])
+
+
+if __name__ == '__main__':
+    unittest.main()
