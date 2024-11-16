069beca6a8fe2ed2affec904a2d1bac27d3a8ff4
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 13:10:04 +0100

    Python Script für sortierung erstellt

---
+++ sort.py
@@ -0,0 +1,8 @@
+# Eine Liste von Zahlen
+zahlen = [34, 1, 56, 23, 89, 12, 7]
+
+# Sortiere die Liste in aufsteigender Reihenfolge
+zahlen.sort()
+
+# Ausgabe der sortierten Liste
+print("Sortierte Liste (aufsteigend):", zahlen)
