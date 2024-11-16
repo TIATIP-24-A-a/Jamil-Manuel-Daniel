ef403efaad9c296b2748dee2e31f7b089debe7ae
DanielHalter <daniel.halter@edu.teko.ch>
2024-11-16 14:34:32 +0100

    Ausgabe für auf und absteigende sortierung

--- sort.py
+++ sort.py
@@ -4,8 +4,11 @@ zahlen: list[int] = [34, 1, 56, 23, 89, 12, 7, 13, 99, 0]
 # Sortiere die Liste in aufsteigender Reihenfolge
 zahlen.sort()
 
+# Ausgabe der sortierten Liste
+print("Sortierte Liste (aufsteigend):", zahlen)
+
 # Sortieren in umgekehrter Reihenfolge
 zahlen.sort(reverse=True)
 
 # Ausgabe der sortierten Liste
-print("Sortierte Liste (aufsteigend):", zahlen)
+print("Sortierte Liste (absteigend):", zahlen)
